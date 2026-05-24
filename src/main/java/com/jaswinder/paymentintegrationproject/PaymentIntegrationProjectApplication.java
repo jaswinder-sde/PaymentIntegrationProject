02fc7a6530092d5d34758448c5683e5988af8153
rishignps <jaswinder.gnps@gmail.com>
2026-05-24 21:05:56 +0530

    Initial Commit

---
+++ src/main/java/com/jaswinder/paymentintegrationproject/PaymentIntegrationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jaswinder.paymentintegrationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentIntegrationProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentIntegrationProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jaswinder/paymentintegrationproject/PaymentIntegrationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaswinder.paymentintegrationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentIntegrationProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
